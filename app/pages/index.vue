7e6ee73039f3d2ae8520e8702eb024c1d225bc60
Ben Sutherland <help@bens.digital>
2025-11-23 15:34:34 +1100

    fix: remove overflow scrolling from the editor

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -32,3 +32,14 @@ onMounted(() => {
     @file-uploaded="handleFileUploaded"
   />
 </template>
+
+<style>
+/* Remove scrollbars from html and body for full-screen editor experience */
+html, body {
+  overflow: hidden;
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 100%;
+}
+</style>
